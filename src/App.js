239276aec1a18cd2e135ea75a4fb412b44efc010
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-11 13:32:00 -0700

    inserted draw fxn into canvas component, in app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import React from "react";
 import Canvas from "./components/Canvas";
 
 function App() {
-  return <Canvas />;
+  //insert drawn fxn into canvas component
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+    ctx.fill();
+  };
+
+  return <Canvas draw={draw} />;
 }
 
 export default App;
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,19 +1,8 @@
 import React, { useRef, useEffect } from "react";
 
 const Canvas = (props) => {
+  const { draw, ...rest } = props;
   const canvasRef = useRef(null);
-
-  //   The frameCount variable: This is a control variable that counts frames.
-  //   If you prefer, you can use a counter for time instead. The goal of this
-  //   variable is provide a clock to our draw function since the animation
-  //   is time dependent.
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = "#000000";
-    ctx.beginPath();
-    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-    ctx.fill();
-  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -21,31 +10,19 @@ const Canvas = (props) => {
     let frameCount = 0;
     let animationFrameId;
 
-    //Draws here
-    // Fxn render: All the steps that will be repeated in
-    // the animation were wrapped in a fxn called render which will be
-    // called recursively by the requestAnimationFrame method.
     const render = () => {
       frameCount++;
-      //   Draw function: The draw function now takes the frame counter
-      //   as argument and the radius of the circle changes over time.
-      //   We also clear the canvas with clearRect function, otherwise it
-      //   would draw over the previous draw every iteration.
       draw(context, frameCount);
       animationFrameId = window.requestAnimationFrame(render);
     };
     render();
 
     return () => {
-      // Cancel animation frame: The function returned in the
-      // useEffect callback (aka clean up function) is called right
-      // before the component un-mount. That way we can ensure that
-      // our animation frame is cancelled after our canvas component un-mount.
       window.cancelAnimationFrame(animationFrameId);
     };
   }, [draw]);
 
-  return <canvas ref={canvasRef} {...props} />;
+  return <canvas ref={canvasRef} {...rest} />;
 };
 
 export default Canvas;
