0fec60bcd8ed873301303e05eba3659d098348dd
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-12 16:01:33 -0700

    checked tree by running node src/story/DecisionTree.js

--- src/story/DecisionTree.js
+++ src/story/DecisionTree.js
@@ -1,5 +1,6 @@
 //import from json
 const story = require("./story.json");
+
 //makes root of tree
 class DecisionTree {
   constructor(node) {
@@ -12,13 +13,14 @@ class Node {
   constructor(story, choiceOneText, choiceTwoText, backgroundImg) {
     this.story = story;
     this.choiceOne = null; //refers to next page (node)
-    this.choiceTwo = null;
+    this.choiceTwo = null; //refers to next page (node)
     this.choiceOneText = choiceOneText; //text on the button
-    this.choiceTwoText = choiceTwoText;
+    this.choiceTwoText = choiceTwoText; //text on the button
     this.backgroundImg = backgroundImg;
   }
 }
 
+//ROOT
 const start = new Node(
   story.startStory.story,
   story.startStory.choiceA,
@@ -26,6 +28,7 @@ const start = new Node(
   "START.jpeg"
 );
 
+//LEFT SIDE OF TREE
 const aStory = new Node(
   story.aStory.story,
   story.aStory.choiceA,
@@ -33,6 +36,7 @@ const aStory = new Node(
   "A.jpeg"
 );
 
+//RIGHT SIDE OF TREE
 const bStory = new Node(
   story.bStory.story,
   story.bStory.choiceA,
@@ -40,6 +44,8 @@ const bStory = new Node(
   "B.jpeg"
 );
 
+//END STORY: WINNER!!!
+//LEFT-left TREE BRANCH
 const cStory = new Node(
   story.cStory.story,
   story.cStory.choiceA,
@@ -47,6 +53,8 @@ const cStory = new Node(
   "C.jpeg"
 );
 
+//END STORY: LOSER!!!
+//LEFT-right TREE BRANCH
 const dStory = new Node(
   story.dStory.story,
   story.dStory.choiceA,
@@ -54,6 +62,8 @@ const dStory = new Node(
   "D.jpeg"
 );
 
+//END STORY: LOSER!!!
+//RIGHT-left TREE BRANCH
 const eStory = new Node(
   story.eStory.story,
   story.eStory.choiceA,
@@ -61,6 +71,8 @@ const eStory = new Node(
   "E.jpeg"
 );
 
+//END STORY: WINNER!!!
+//RIGHT-right TREE BRANCH
 const fStory = new Node(
   story.fStory.story,
   story.fStory.choiceA,
