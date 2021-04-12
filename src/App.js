ab299518f339634499b92c16aba22fee99e82315
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-12 16:51:37 -0700

    moved start page into its own component file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import React from "react";
 // import Canvas from "./components/Canvas";
-import candle from "./game-imgs/candle.gif";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+
+//Route to Page Imports
+import startPg from "./components/startPg";
 
 function App() {
   //////////////////Logical Size Fxn///////////////////////////////
@@ -62,16 +65,13 @@ function App() {
   // };
 
   return (
-    <div className="app-main-container">
-      <h1>Choose Your Own Adventure!</h1>
-      <h2 className="h2-title-text">The Tale of a Wondering Soul</h2>
-      <h3 className="h3-title-text">In the Withering Kingdom of Halyth</h3>
-      <img src={candle} alt="flickeringCandle" className="candle" />
+    <BrowserRouter>
       <div>
-        <button className="simple-button">Begin Adventure</button>
+        <Switch>
+          <Route path="/" component={startPg} exact={true} />
+        </Switch>
       </div>
-      {/* <Canvas draw={draw} className="canvas-one" /> */}
-    </div>
+    </BrowserRouter>
   );
 }
 
