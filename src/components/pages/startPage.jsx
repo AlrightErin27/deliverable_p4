f5173827affa28653f9f4d4f18adbf5b51d3416e
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-13 11:01:21 -0700

    added grid to start page lay out. now sacles text and image with page size.

--- src/components/pages/startPage.jsx
+++ src/components/pages/startPage.jsx
@@ -3,8 +3,9 @@ import story from "../../story/story.json";
 const startPage = () => (
   <div className="startPg-container">
     <div className="story-text">
-      <p>{story.startStory.story}</p>
+      <p className="paragraph">{story.startStory.story}</p>
     </div>
+    <div className="start-canvas"></div>
   </div>
 );
 export default startPage;
