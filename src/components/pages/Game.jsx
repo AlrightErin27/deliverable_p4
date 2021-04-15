5d91bd88710ba9d8b258676d65e863e3b027c5ed
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-15 14:55:24 -0700

    lil man onthe page

--- src/components/pages/Game.jsx
+++ src/components/pages/Game.jsx
@@ -1,53 +1,27 @@
 import tree from "../../story/DecisionTree";
 import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import Canvas from "../Canvas";
-import warrior from "../../warrior/walk/WALK0.png";
 
 const Game = () => {
   //set State
   const [gameState, setGameState] = useState(tree.root);
-  const myCanvas = useRef(null);
 
-  // useEffect(() => {
-  //   updateCanvas();
-  // }, []);
+  const [frame, setFrame] = useState(0);
 
-  const updateCanvas = () => {
-    const ctx = this.refs.myCanvas.getContext("2d");
-    const img = new Image();
-    img.src = warrior;
-    img.onload = function () {
-      ctx.drawImage(img, 40, 40);
+  useEffect(() => {
+    const incrementFrame = () => {
+      console.log(`./warrior/${gameState.animation}/WALK${frame}.png`);
+      if (frame === 9) {
+        setFrame(0);
+      } else {
+        setFrame(frame + 1);
+      }
     };
-  };
-
-  //fxn makes canvas controllable via css
-  function resizeCanvasToDisplaySize(canvas) {
-    const { width, height } = canvas.getBoundingClientRect();
-    if (canvas.width !== width || canvas.height !== height) {
-      canvas.width = width;
-      canvas.height = height;
-      return true; // here you can return some useful info like delta width and
-      //delta height instead of just true
-      // this information can be used in the next redraw...
-    }
-    return false;
-  }
-
-  //insert drawn fxn into CANVAS component
-  //   Draw fxn: Takes the frame counter
-  //   as argument & radius of the circle changes/ time.
-  //   We also clear the canvas with clearRect fxn, otherwise it
-  //   would draw over the previous draw every iteration.
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = "#000000";
-    ctx.beginPath();
-
-    // ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-    ctx.fill();
-  };
+    const interval = setInterval(incrementFrame, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [frame]);
 
   return (
     <div className="story-container">
@@ -58,9 +32,12 @@ const Game = () => {
       <div className="story-background">
         <img src={gameState.backgroundImg} alt="background" />
       </div>
-
-      {/* ------------------------------CANVAS HERE----------------------- */}
-      <Canvas ref={myCanvas} draw={draw} className="canvas" />
+      <div className="animation">
+        <img
+          src={`./warrior/${gameState.animation}/WALK${frame}.png`}
+          alt="animation"
+        />
+      </div>
 
       <div className="button-container">
         {gameState.choiceOneText && (
